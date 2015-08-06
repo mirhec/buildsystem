0e46219207ef81cf2aaffb8964a42b71a7371adf
Mirko Hecky <mh@simon-ibv.de>
2015-08-06 11:10:33 +0200

    Pfade fuer msdev angepasst, so dass out_dir nun ausgehend von dem current working dir gesehen wird.

--- msdev.py
+++ msdev.py
@@ -8,20 +8,22 @@ class MsdevBuilder(BaseBuilder):
     solution_file = None
     target = None
     out_dir = 'bin'
-    
+
     @task('compile')
     def compile_it(self):
         '''Compiles MSDev solution.'''
         if self.solution_file and self.target:
-            self.run([self.msbuild_exe, '/t:build', '/p:Configuration=Release;TargetName=%s' % self.target, '/property:OutDir=%s' % self.out_dir, self.solution_file])
-            
+            me = os.getcwd()
+            print me + '/' + self.out_dir
+            self.run([self.msbuild_exe, '/t:build', '/p:Configuration=Release;TargetName=%s' % self.target, '/property:OutDir=%s' % me + '/' + self.out_dir, self.solution_file])
+
             # now move it to the right location and rename it
             base = os.path.join(self.out_dir, self.target)
             exts_to_move = ['.dll', '.exe', '.lib']
             for f in exts_to_move:
                 if os.path.exists(base + f):
                     shutil.move(base + f, base + '-' + self.version + f)
-    
+
     @task('clean')
     def clean(self):
         files = os.listdir(self.out_dir)
