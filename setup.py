0474045a7647dcf0078c78ad54e71fc88d60c3cf
Mirko Hecky <mh@simon-ibv.de>
2016-02-22 09:49:25 +0100

    Changed setup build script in order to add git.txt file with version infos

--- setup.py
+++ setup.py
@@ -14,5 +14,13 @@ class SetupBuilder(BaseBuilder):
         if self.setupscript and os.path.exists(os.path.join(self.setupdir, self.setupscript)):
             d = os.getcwd()
             os.chdir(self.setupdir)
+            # write version information into git.txt
+            with open('git.txt', 'w') as f:
+                f.write(self.version)
+                
+            # run setup
             self.run([r'C:\Program Files (x86)\Inno Setup 5\ISCC.exe', '/cc', self.setupscript])
+            
+            # remove git.txt
+            os.remove('git.txt')
             os.chdir(d)
