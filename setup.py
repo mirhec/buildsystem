6e642bbcbdbb3a4f1ac6cc83db9404e827179609
Mirko Hecky <mh@simon-ibv.de>
2017-03-24 10:05:28 +0100

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,59 +1,12 @@
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+#!/usr/bin/env python
 
-here = path.abspath(path.dirname(__file__))
+from distutils.core import setup
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name='buildsystem',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
-
-    description='A very simple, python-based and extendable buildsystem.',
-    long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/mirhec/buildsystem',
-
-    # Author details
-    author='Mirko Hecky',
-
-    # What does your project relate to?
-    keywords='buildsystem build buildtool tool',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': [],
-        'test': [],
-    },
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
-)
+setup(name='buildsystem',
+      version='1.0',
+      description='A very simple, python-based and extendable buildsystem.',
+      author='Mirko Hecky',
+    #   author_email='gward@python.net',
+      url='https://github.com/mirhec/buildsystem',
+      packages=['src', 'examples'],
+     )
