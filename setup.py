4faee32780f499588a237e17012a128387eb8c16
Mirko Hecky <mh@simon-ibv.de>
2017-03-24 10:09:20 +0100

    Removed email-comment from setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='buildsystem',
       version='1.0',
       description='A very simple, python-based and extendable buildsystem.',
       author='Mirko Hecky',
-    #   author_email='gward@python.net',
       url='https://github.com/mirhec/buildsystem',
       packages=['buildsystem', 'buildsystem.examples'],
-     )
+)
