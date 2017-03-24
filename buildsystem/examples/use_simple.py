23151919e599ecdfa357ad091a84500969e7ecaa
Mirko Hecky <mh@simon-ibv.de>
2017-03-24 10:14:00 +0100

    Removed overhead from use_simple.py

--- buildsystem/examples/use_simple.py
+++ buildsystem/examples/use_simple.py
@@ -1,11 +1,3 @@
-# Prepare the buildsystem
-from subprocess import Popen, call
-from os.path import exists
-if exists('buildsystem'):
-    Popen(['git', 'pull', 'origin'], cwd='buildsystem').wait()
-else:
-    call(['git', 'clone', 'https://github.com/mirhec/buildsystem', 'buildsystem'])
-
 # Import the builder
 from buildsystem.simple import MyBuilder
 
