92e3d8aacecad4f143dc3f08239a9642b91ce5ab
Mirko Hecky <m.hecky@simon-ibv.de>
2017-04-04 15:38:43 +0200

    Added license file and some changes to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(name='buildsystem',
       description='A very simple, python-based and extendable buildsystem.',
       author='Mirko Hecky',
       url='https://github.com/mirhec/buildsystem',
+      download_url='https://github.com/mirhec/buildsystem/archive/1.0.tar.gz',
       packages=['buildsystem', 'buildsystem.examples'],
+      keywords=['buildsystem', 'build', 'automation']
 )
