1256e37294da687ecc2227bfacb6f43c125c8d48
Mirko Hecky <m.hecky@simon-ibv.de>
2017-04-04 17:15:56 +0200

    Updated version info to 1.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='buildsystem',
-      version='1.0',
+      version='1.1',
       description='A very simple, python-based and extendable buildsystem.',
       author='Mirko Hecky',
       url='https://github.com/mirhec/buildsystem',
-      download_url='https://github.com/mirhec/buildsystem/archive/1.0.tar.gz',
+      download_url='https://github.com/mirhec/buildsystem/archive/1.1.tar.gz',
       packages=['buildsystem', 'buildsystem.examples'],
       keywords=['buildsystem', 'build', 'automation']
 )
