2960a80176325d8c50fa26648ee952ed364069f3
Mirko Hecky <m.hecky@simon-ibv.de>
2017-09-26 08:50:32 +0200

    Created version 1.6

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='buildsystem',
-      version='1.5',
+      version='1.6',
       description='A very simple, python-based and extendable buildsystem.',
       author='Mirko Hecky',
       url='https://github.com/mirhec/buildsystem',
-      download_url='https://github.com/mirhec/buildsystem/archive/1.5.tar.gz',
+      download_url='https://github.com/mirhec/buildsystem/archive/1.6.tar.gz',
       packages=['buildsystem', 'buildsystem.examples'],
       keywords=['buildsystem', 'build', 'automation']
 )
